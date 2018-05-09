07a5c456839aa8ff5ea693cb0eb84a9e9803d6c8
Thomas Wyns <32387294+Thomaach@users.noreply.github.com>
2018-05-09 20:49:05 +0200

    aanpassingen en verbeteringen

--- Project-test/script.js
+++ Project-test/script.js
@@ -6,7 +6,7 @@ $(function () {
     var anim_id,
         game_over = false,
         score_counter = 1,
-        speed = 2,
+        speed = 3,
         line_speed = 5,
         car = $('#car'),
         car_1 = $('#car_1'),
@@ -15,9 +15,12 @@ $(function () {
         line_1 = $('#line_1'),
         line_2 = $('#line_2'),
         line_3 = $('#line_3'),
+        line_4 = $('#line_4'),
         restart_div = $('#restart_div'),
         restart_btn = $('#restart'),
         score = $('#score'),
+        highscore = $('#highscore'),
+        highscore_counter = 0,
         container = $('#container'),
         container_left = parseInt(container.css('left')),
         container_width = parseInt(container.width()),
@@ -27,107 +30,18 @@ $(function () {
         move_right = false,
         move_left = false,
         move_up = false,
-        move_down = false,
-        mobiel = detectmob();
+        move_down = false;
 
-
-
-
-    // device detection
-    function detectmob() {
-        if (navigator.userAgent.match(/Android/i) ||
-            navigator.userAgent.match(/webOS/i) ||
-            navigator.userAgent.match(/iPhone/i) ||
-            navigator.userAgent.match(/iPad/i) ||
-            navigator.userAgent.match(/iPod/i) ||
-            navigator.userAgent.match(/BlackBerry/i) ||
-            navigator.userAgent.match(/Windows Phone/i)
-        ) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-
-
-
-    function watTeGebruiken() {
-        if (mobiel === false) {
-            $("#links").mousedown(function () {
-                if (game_over === false) {
-                    move_left = requestAnimationFrame(left);
-                }
-            })
-
-            $("#links").mouseup(function () {
-                if (game_over === false) {
-                    cancelAnimationFrame(move_left);
-                    move_left = false;
-                }
-            })
-
-            $("#rechts").mousedown(function () {
-                if (game_over === false) {
-                    move_right = requestAnimationFrame(right);
-                }
-            })
-
-            $("#rechts").mouseup(function () {
-                if (game_over === false) {
-                    cancelAnimationFrame(move_right);
-                    move_right = false;
-                }
-            })
-        } else {
-            $("#links").touchstart(function () {
-                if (game_over === false) {
-                    move_left = requestAnimationFrame(left);
-                }
-            })
-
-            $("#links").touchcancel(function () {
-                if (game_over === false) {
-                    cancelAnimationFrame(move_left);
-                    move_left = false;
-                }
-            })
-
-            $("#rechts").touchstart(function () {
-                if (game_over === false) {
-                    move_right = requestAnimationFrame(right);
-                }
-            })
-
-            $("#rechts").touchcancel(function () {
-                if (game_over === false) {
-                    cancelAnimationFrame(move_right);
-                    move_right = false;
-                }
-            })
-        }
-    }
 
     // Bewegen
 
 
-
-
-
-
-
-
     function left() {
         if (game_over === false && parseInt(car.css('left')) > 0) {
-
             car.css('left', parseInt(car.css('left')) - 5);
             move_left = requestAnimationFrame(left);
         }
     }
-
-
-
 
     function right() {
         if (game_over === false && parseInt(car.css('left')) < container_width - car_width) {
@@ -137,31 +51,48 @@ $(function () {
     }
 
 
-    /* Misschien niet toevoeg wordt later nog bekeken
-    function up() {
-        if (game_over === false && parseInt(car.css('top')) > 0) {
-            car.css('top', parseInt(car.css('top')) - 3);
-            move_up = requestAnimationFrame(up);
-        }
+    function leftend() {
+        cancelAnimationFrame(move_left);
+        move_left = false;
     }
-    $("#omhoog").click(function () {
+
+    function rightend() {
+        cancelAnimationFrame(move_right);
+        move_right = false;
+    }
+
+    document.getElementById("links").addEventListener("touchstart", left);
+    document.getElementById("rechts").addEventListener("touchstart", right);
+
+    document.getElementById("links").addEventListener("touchend", leftend);
+    document.getElementById("rechts").addEventListener("touchend", rightend);
+
+
+    $("#links").mousedown(function () {
         if (game_over === false) {
-            move_up = requestAnimationFrame(up);
+            move_left = requestAnimationFrame(left);
         }
     })
 
-    function down() {
-        if (game_over === false && parseInt(car.css('top')) < container_height - car_height) {
-            car.css('top', parseInt(car.css('top')) + 3);
-            move_down = requestAnimationFrame(down);
-        }
-    }
-    $("#omlaag").click(function () {
+    $("#links").mouseup(function () {
         if (game_over === false) {
-            move_down = requestAnimationFrame(down);
+            leftend();
         }
     })
-    */
+
+    $("#rechts").mousedown(function () {
+        if (game_over === false) {
+            move_right = requestAnimationFrame(right);
+        }
+    })
+
+    $("#rechts").mouseup(function () {
+        if (game_over === false) {
+            rightend();
+        }
+    })
+
+
 
     anim_id = requestAnimationFrame(repeat);
 
@@ -171,14 +102,14 @@ $(function () {
             return;
         }
 
-        score_counter++;
+        score_counter ++;
 
         if (score_counter % 20 == 0) {
             score.text(parseInt(score.text()) + 1);
         }
         if (score_counter % 500 == 0) {
-            speed++;
-            line_speed++;
+            speed + 1;
+            line_speed + 1;
         }
 
         car_down(car_1);
@@ -188,10 +119,10 @@ $(function () {
         line_down(line_1);
         line_down(line_2);
         line_down(line_3);
+        line_down(line_4);
 
         anim_id = requestAnimationFrame(repeat);
     }
-
 
     //de auto's laten zakken
     function car_down(car) {
@@ -243,15 +174,24 @@ $(function () {
         cancelAnimationFrame(anim_id);
         cancelAnimationFrame(move_right);
         cancelAnimationFrame(move_left);
-        cancelAnimationFrame(move_up);
-        cancelAnimationFrame(move_down);
         restart_div.slideDown();
         restart_btn.focus();
+        /* if (score_counter > highscore_counter) {
+             highscore.text(highscore);
+         }*/
+        
+
     }
 
-    //herstarten spel
-    restart_btn.click(function () {
+    //herstarten spel Moet nog werken
+
+    restart_btn.click(function() {
         location.reload();
     });
+    
+    
+    document.getElementById("restart").addEventListener("touchstart", location.reload);
+    
+
 
 });
